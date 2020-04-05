b6cb512b16f52250b2abeeb0dfd32471c247f64f
Matheus Stolet <matheusstolet@hotmail.com>
2020-04-04 18:28:46 -0700

    Sort so that mask and image data are the same

--- dataset.py
+++ dataset.py
@@ -36,6 +36,8 @@ class Killer_Whale_Dataset(Dataset):
             self.mask_list = np.load("data/masks.npy", allow_pickle=True)
         else:
             self.img_list, self.mask_list = Killer_Whale_Dataset._load_dataset(data_folder)
+            self.img_list.sort(key=lambda x: x["path"])
+            self.mask_list.sort(key=lambda x: x["path"])
             np.save("data/imgs.npy", self.img_list)
             np.save("data/masks.npy", self.mask_list)
 
@@ -52,7 +54,7 @@ class Killer_Whale_Dataset(Dataset):
         
         return {"img": img_array, 
                 "img_id": img["id"], 
-                "img_species": img["species"], 
+                "img_species": img["species"],
                 "mask": mask_array, 
                 "mask_id": mask["id"], 
                 "mask_species": mask["species"]}
@@ -69,12 +71,17 @@ class Killer_Whale_Dataset(Dataset):
                 whale = {}
                 filepath = subdir + "/" + f
                 
+                # There is no mask for picture KW6CA171B_6.jpg so skip it for now
+                if "KW6CA171B_6.jpg" in filepath:
+                    continue
+                
                 id = filepath.split("/")[2]
                 species =  filepath.split("/")[1]
                 img_or_mask = filepath.split("/")[3]
-                
+            
                 whale["id"] = id
-                
+                whale["path"] = filepath
+
                 if "resident" in species:
                     whale["species"] = "resident"
                 elif "transient" in species:
@@ -82,22 +89,23 @@ class Killer_Whale_Dataset(Dataset):
 
                 img = Image.open(filepath)
                 img = img.resize((600, 600))
-                if img_or_mask == "img":
+                if img_or_mask == "img" or img_or_mask == "IMG":
                     whale["img"] = img 
                     imgs.append(whale)
                 elif img_or_mask == "mask":
                     whale["mask"] = img
                     masks.append(whale)
         
-        return imgs, masks
+        return imgs, masks 
 
-
-transform = transforms.Compose([transforms.ToTensor()])
-
-whale_path = 'data/'
-
-whale_data = Killer_Whale_Dataset(whale_path, transform=transform)
-print(whale_data.__len__())
-print(type(whale_data[0]))
-plt.imshow(transforms.ToPILImage()(whale_data[0]['img']))
-plt.show()
+#transform = transforms.Compose([transforms.ToTensor()])
+#
+#whale_path = 'data/'
+#
+#whale_data = Killer_Whale_Dataset(whale_path, transform=transform)
+#print(whale_data.__len__())
+#print(type(whale_data[0]))
+#plt.imshow(transforms.ToPILImage()(whale_data[268]['img']))
+#plt.show()
+#plt.imshow(transforms.ToPILImage()(whale_data[268]['mask']))
+#plt.show()
